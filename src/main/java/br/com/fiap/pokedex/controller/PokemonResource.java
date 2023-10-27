894f6eb835b590c5792ef80c2f5664d3a71a0ddb
vgont <vgontijo34@gmail.com>
2023-10-27 17:36:54 -0300

    crud finalized

--- src/main/java/br/com/fiap/pokedex/controller/PokemonResource.java
+++ src/main/java/br/com/fiap/pokedex/controller/PokemonResource.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 
 import br.com.fiap.pokedex.model.entity.Pokemon;
 import br.com.fiap.pokedex.model.repository.PokemonRepository;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -13,13 +18,46 @@ import jakarta.ws.rs.core.Response.ResponseBuilder;
 
 @Path("/pokedex")
 public class PokemonResource {
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAll() {
-		ArrayList<Pokemon> resposta = PokemonRepository.findAll();
-		ResponseBuilder response = Response.ok();
-		response.entity(resposta);
-		return response.build();
+		ArrayList<Pokemon> foundedPokemons = PokemonRepository.findAll();
+
+		if (foundedPokemons.isEmpty()) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return Response.ok(foundedPokemons).build();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response save(Pokemon pokemon) {
+		Pokemon savedPokemon = PokemonRepository.save(pokemon);
+
+		if (savedPokemon == null) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		return Response.created(null).entity(savedPokemon).build();
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(Pokemon pokemon) {
+		Pokemon updatedPokemon = PokemonRepository.update(pokemon);
+
+		if (updatedPokemon == null) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		return Response.created(null).entity(updatedPokemon).build();
+	}
+
+	@DELETE
+	@Path("/{numero}")
+	public Response delete(@PathParam("numero") Long numero) {
+		if (PokemonRepository.delete(numero)) {
+			return Response.noContent().build();
+		}
+		return Response.status(Response.Status.BAD_REQUEST).build();
 	}
 }
--- src/main/java/br/com/fiap/pokedex/model/repository/PokemonRepository.java
+++ src/main/java/br/com/fiap/pokedex/model/repository/PokemonRepository.java
@@ -1,5 +1,6 @@
 package br.com.fiap.pokedex.model.repository;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,30 +13,103 @@ public class PokemonRepository extends Repository {
 	public static ArrayList<Pokemon> findAll() {
 		ArrayList<Pokemon> pokemons = new ArrayList<Pokemon>();
 		String sql = "select * from tb_ddd_pokedex";
-		try {
-			PreparedStatement ps = getConnection().prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
+
+		try (PreparedStatement ps = getConnection().prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
 			while (rs.next()) {
-				Pokemon pokemon = createPokemon(rs);
+				Pokemon pokemon = createPokemonWithResultSet(rs);
 				pokemons.add(pokemon);
 			}
 			return pokemons;
+
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
-		} finally {
-			closeConnection();
 		}
 		return null;
 	}
 
-	private static Pokemon createPokemon(ResultSet rs) throws SQLException {
-		Pokemon pokemon = new Pokemon();
-		pokemon.setNumero(rs.getLong("numero"));
-		pokemon.setNome(rs.getString("nome"));
-		pokemon.setAltura(rs.getDouble("altura"));
-		pokemon.setPeso(rs.getDouble("peso"));
-		pokemon.setCategoria(rs.getString("categoria"));
-		pokemon.setDataDaCaptura(rs.getDate("data_da_caputura").toLocalDate());
-		return pokemon;
+	public static Pokemon save(Pokemon pokemon) {
+		String sql = "insert into tb_ddd_pokedex(numero,nome,altura,peso,categoria,data_da_captura) "
+				+ "values(null,?,?,?,?,?)";
+
+		try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+
+			setPreparedStatementParameters(ps, pokemon, false);
+
+			int rowsAffected = ps.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				return pokemon;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	public static Pokemon update(Pokemon pokemon) {
+		String sql = "update tb_ddd_pokedex set nome=?, altura=?, peso=?, categoria=?, data_da_captura=?"
+					+ "where numero=?";
+		
+		try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+			setPreparedStatementParameters(ps, pokemon, true);
+			
+			int rowsAffected = ps.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				return pokemon;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+
+	public static boolean delete(Long numero) {
+		String sql = "DELETE FROM tb_ddd_pokedex WHERE numero=?";
+
+		try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+			ps.setLong(1, numero);
+
+			int rowsAffected = ps.executeUpdate();
+			
+			return rowsAffected > 0;
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}		
+		return false;
+	}
+
+	private static Pokemon createPokemonWithResultSet(ResultSet rs) {
+		try {
+			Pokemon pokemon = new Pokemon();
+			pokemon.setNumero(rs.getLong("numero"));
+			pokemon.setNome(rs.getString("nome"));
+			pokemon.setAltura(rs.getDouble("altura"));
+			pokemon.setPeso(rs.getDouble("peso"));
+			pokemon.setCategoria(rs.getString("categoria"));
+			pokemon.setDataDaCaptura(rs.getDate("data_da_captura").toLocalDate());
+			return pokemon;
+		} catch (SQLException e) {
+			System.out.println("Erro ao criar pokemon: " + e.getMessage());
+		}
+		return null;
+	}
+	
+	private static void setPreparedStatementParameters(PreparedStatement ps, Pokemon pokemon, boolean isUpdate) {
+		try {
+			ps.setString(1, pokemon.getNome());
+			ps.setDouble(2, pokemon.getAltura());
+			ps.setDouble(3, pokemon.getPeso());
+			ps.setString(4, pokemon.getCategoria());
+			ps.setDate(5, Date.valueOf(pokemon.getDataDaCaptura()));
+			
+			if (isUpdate) {
+				ps.setLong(6, pokemon.getNumero());
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
